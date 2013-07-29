25b35ba993ab0e4c0f2dd140e9a8d4799a72a18d
Maximilien Rzepka <maximilien.rzepka@gmail.com>
2013-07-29 11:09:41 +0200

    add requirements and copy code from cascalog for impatient

--- nba/src/nba/core.clj
+++ nba/src/nba/core.clj
@@ -22,6 +22,71 @@
   (s/split line #"[;,\s]+"))
 
 
+;;Extract from json tweets the following tweets :
+;;   - extract id , lang , text , hastag, mention
+;;   - cleanup text field : replace \n and | to other characters
+;;   - ??howto?? extract first line and extract keywords used for the search
+;;   - find keywords in text --> new field keywords
+;;   - language detection --> new field reallang
+;;   - remove stopwords, punctuation , hyperlinks --> new field terms
+
+(defn etf-tweet
+  [in out & {:keys [delimiter trap] :or {delimiter \| trap "error"} :as opts}]
+  (ca/<- [?doc-id ?word]
+      (in ?doc-id ?line)
+      (split ?line :> ?word-dirty)
+      ((co/comp s/trim s/lower-case) ?word-dirty :> ?word)
+      #_(stop ?word :> false))
+)
+
+(defn word-count [src]
+  "simple word count across all documents"
+  (ca/<- [?word ?count]
+      (src _ ?word)
+      (co/count ?count)))
+
+;; TD-IDF as it is in Cascalog for the impatient
+;; https://github.com/Quantisan/Impatient/blob/cascalog/part6/src/impatient/core.clj
+
+#_(defn D [src]
+  (let [src  (select-fields src ["?doc-id"])]
+    (<- [?n-docs]
+        (src ?doc-id)
+        (c/distinct-count ?doc-id :> ?n-docs))))
+
+#_(defn DF [src]
+  (<- [?df-word ?df-count]
+      (src ?doc-id ?df-word)
+      (c/distinct-count ?doc-id ?df-word :> ?df-count)))
+
+#_(defn TF [src]
+  (<- [?doc-id ?tf-word ?tf-count]
+      (src ?doc-id ?tf-word)
+      (c/count ?tf-count)))
+
+(defn tf-idf-formula [tf-count df-count n-docs]
+  (->> (+ 1.0 df-count)
+    (ca/div n-docs)
+    (Math/log)
+    (* tf-count)))
+
+#_(defn TF-IDF [src]
+  (let [n-doc (first (flatten (??- (D src))))]
+    (<- [?doc-id ?tf-idf ?tf-word]
+        ((TF src) ?doc-id ?tf-word ?tf-count)
+        ((DF src) ?tf-word ?df-count)
+        (tf-idf-formula ?tf-count ?df-count n-doc :> ?tf-idf))))
+
+
+;; Some Stats :
+;;   - terms frequency --> find relevant terms in this context (game, win, heat)
+;;   - most found search keywords
+;;   - most used hashtags
+;;   - most used entities
+;;   - compute TD-IDF
+;; Goal : from stats determine manually most relevant terms and classify them
+;;   - classes : spurs , miamiheat , both , none , positive , neutral , negative
+;;
 (defn most-frequent-terms
   [in out & {:keys [delimiter trap] :or {delimiter \| trap "error"} :as opts}]
   (let [count-q (ca/<- [?term ?count]         
@@ -32,3 +97,9 @@
           (:trap (ca/lfs-textline trap)))
         q (co/first-n count-q 20 :sort ["?count"] :reverse true)]
     (ca/??- q)))
+
+;; Some Analysis (only on english tweet ? ) :
+;;   - unsupervised clustering based on TD-IDF
+;;   - sentiment analysis on tweet
+;;   - based on terms classes, perform clustering
+;;   - supervised classification ( spurs , heat , neutral , both)
