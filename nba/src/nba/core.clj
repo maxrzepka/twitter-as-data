402af87aabc100503fb6271ef7c6cd3e898f132e
Maximilien Rzepka <maximilien.rzepka@gmail.com>
2013-07-31 01:19:39 +0200

    add requirements, add conversion text to terms

--- nba/src/nba/core.clj
+++ nba/src/nba/core.clj
@@ -16,33 +16,86 @@
   (json/read-str line :key-fn keyword))
 
 (ca/defmapcatop split [line]
-  "reads string and splits it by regex"
-  (s/split line #"[;,\s]+"))
+  "reads string and splits it by space"
+  (s/split line #"[\s]+"))
+
+(def punctuation "[](){}:;?!\",. \t\r\n")
+(def punc-regexp #"[\[\]\(\)\{\}:?!;,\s]+")
+;; 
+(ca/defmapcatop split-by-punc [text]
+  "split text by any kind of punctuation"
+  (s/split text punc-regexp))
+
+(defn delete-punct [s]
+  (apply str
+         (filter (comp not (set punctuation)) s)))
+
+;;TODO handle case where emoticon juxt word like "interesting:)"
+;; possible trim-split logic : 
+;;   
+(defn scrub-text [s]
+  "trim open punctuation and lower case"
+  ((comp delete-punct s/lower-case) s))
+
+;; detect unicode string ,clean and convert it to meaning
+;;   - http://rishida.net/tools/conversion/ app to test unicode strings
+;; TODO get full list of unicode pictographs...
+;;   - unicode databases
+;;      - http://www.charbase.com/
+;;      - http://www.fileformat.info/info/unicode/char/2764/index.htm
+;; \u261d means WHITE UP POINTING INDEX
+;; \u2764 means HEAVY BLACK HEART
+;; remove invalid unicode [only the last is a good one]...
+
+;; TODO detect unicode smiley
+(defn unicode-smiley? [s]
+  true)
+
+;; TODO Store all emoticons known + match them with word :
+;; ===))) is equivalent to =) ...
+(defn emoticon?
+  "Guess if a string is a smiley.
+What is a smailey ?
+Naive answer : any string containing only a given set of characters
+"
+  [^String s]
+  (let [letters (set ";:,()PbD-_^$/3L'")]
+    (and
+     (> (.length s) 1)
+     (every? letters s))))
+
+;; classify smiley in sentiment category : positive/neutral/negative
+;; Exploiting Emoticons in Sentiment Analysis
+;;  http://eprints.eemcs.utwente.nl/23268/
+;; Does Size Matter? Text and Grammar Revision for Parsing Social Media Data
+;; http://cl.indiana.edu/~md7/papers/khan-et-al13.pdf
+
+(defn hyperlink? [^String s]
+  (boolean (re-find #"^(https?|ftp):" s)))
+
+;; query to split twitter text into terms :
+;;    - remove stopwords
+(defn ->terms [text]
+  (ca/<- [?term]
+         (hyperlink? ?term :> false)
+         (scrub-text ?word :> ?term)
+         ;;TODO add word split for ascii emoticon
+         (split text :> ?word)
+         ([text] ?text)))
+
+;; query to find keywords in terms
+;; keywords are coming from file
+
+(defn build-extract-join
+  "Returns a function on maps : get-in _ ks + map f _ + join sep _"
+  ([ks] (build-extract-join ks identity " "))
+  ([ks f] (build-extract-join ks f " "))
+  ([ks f sep]
+     (fn [m]
+       (s/join sep (map f (get-in m ks))))))
 
 ;; Extract from json tweets the following tweets :
 ;;   - extract id , lang , text , hastag, mention
-;;   - cleanup text field : replace \n and | to other characters
-;;   - ??howto?? extract first line and extract keywords used for the search
-;;   - find keywords in text --> new field keywords
-;;   - language detection --> new field reallang
-;;   - remove stopwords, punctuation , hyperlinks --> new field terms
-
-;; High-order function
-#_(ca/defmapop [extractor [rules]] [value]
-  (map (fn [rule] (get-in value rule)) rules))
-
-#_((extractor [[:id] [:lang] [:text]
-                      [:entities :hashtags]
-                      [:entities :user_mentions]])
-          ?tweet
-          :> ?id ?lang ?text ?hashtags ?mentions)
-
-(defn build-extract-join [ks f]
-  (fn [m]
-    (s/join " " (map f (get-in m ks)))))
-
-;; Fct that returns a fct 
-
 (defn extractor [tweet]
   ((juxt :id :lang :text
          (build-extract-join [:entities :hashtags] :text)
